1d8612514aba46c8dc634a236f9ba5a8e82fc69f
amuammer <amuammer@students.iugaza.edu.ps>
2020-06-29 17:06:25 +0300

    add reset on startup

--- js/profile.js
+++ js/profile.js
@@ -1,15 +1,21 @@
 
 const resetButton = document.getElementById("reset");
 
-resetButton.addEventListener("click", function(e) {
+const resetExcercises = function(e) {
     Exercise.reset();
     // save to localStorage
     Exercise.save();
     // redner All
     Exercise.renderExercisesTable();
-});
+}
 
+resetButton.addEventListener("click", resetExcercises);
 
 Exercise.load();
+if (exercises.length) { // if not exercises => reset them
+  Exercise.renderExercisesTable();
+} else {
+  resetExcercises();
+}
+
 console.log(exercises);
-Exercise.renderExercisesTable();
